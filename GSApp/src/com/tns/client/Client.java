ea44bdee9d59d82019e363d1265e833ede782997
Ravi N <94108095+RaviN252@users.noreply.github.com>
2022-11-23 13:00:11 +0530

    Update Client.java

--- GSApp/src/com/tns/client/Client.java
+++ GSApp/src/com/tns/client/Client.java
@@ -1,5 +1,4 @@
 package com.tns.client;
-
 import com.tns.application.GSNormalAcc;
 import com.tns.application.GSPrimeAcc;
 import com.tns.application.GSShopFactory;
@@ -7,18 +6,14 @@ import com.tns.framework.NormalAcc;
 import com.tns.framework.PrimeAcc;
 import com.tns.framework.ShopAcc;
 import com.tns.framework.ShopFactory;
-
-
 public class Client 
 {
 	public static void main(String[] args) 
 	{
 		// Step 1 - Step a
 		GSShopFactory gssfactory = new GSShopFactory();
-		gssfactory.getNewPrimeAccount(236, "Ravi", 1000, true);
-		gssfactory.getNewNormalAccount(785, "RAvin", 1000, 50);
-		
-		
+		gssfactory.getNewPrimeAccount(123, "Ravi", 1000, true);
+		gssfactory.getNewNormalAccount(234, "RaviN", 1000, 50);
 		// Step 2
 		GSPrimeAcc gsprime = new GSPrimeAcc();
 		gsprime.getAccNm();
@@ -26,24 +21,17 @@ public class Client
 		gsprime.getCharges();
 		// Step 4 / Step d.
 		gsprime.bookProduct(1000);
-		
 		// Step 5 / Step e.
 		gsprime.toString();
-		
 		// Step 3
 		GSNormalAcc gsnormal = new GSNormalAcc(); 
 		gsnormal.getAccNm();
 		gsnormal.getAccNo();
 		gsnormal.getCharges();
 		gsnormal.getDeliveryCharge();
-		
 		// Step 4 / Step d.
 		gsnormal.bookProduct(1000);
 		// Step 5 / Step e.
 		gsnormal.toString();
-	
-		
-
 	}
-
 }
